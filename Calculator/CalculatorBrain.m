55628b484ae14d18de41e9be212923a6ac0de436
William Lee <github@william-lee.com>
2011-09-12 11:35:36 +0100

    completed view and controller stubs(header)

---
+++ Calculator/CalculatorBrain.m
@@ -0,0 +1,23 @@
+//
+//  CalculatorBrain.m
+//  Calculator
+//
+//  Created by William Lee on 12/09/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "CalculatorBrain.h"
+
+@implementation CalculatorBrain
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
